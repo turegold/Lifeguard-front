1cf244ddaa9dd2afaee177006a242a21f79c553c
이용민 <bacaren21@naver.com>
2026-01-15 16:35:49 +0900

    에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
             proxy.on('error', (err, _req, _res) => {
               console.log('proxy error', err)
             })
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
+            proxy.on('proxyReq', (_proxyReq, req, _res) => {
               console.log(
                 'Sending Request to the Target:',
                 req.method,
